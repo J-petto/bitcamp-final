1e3760a27646d04571103a346a72975789682615
황민지 <hminji4565@gmail.com>
2024-12-06 18:25:03 +0900

    포트 삭제

--- app/src/main/java/bitcamp/project/config/WebConfig.java
+++ app/src/main/java/bitcamp/project/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000, http://211.188.57.118, http://211.188.57.118:3000")  // 리액트 앱이 실행되는 주소
+            .allowedOrigins("http://localhost:3000, http://211.188.57.118")  // 리액트 앱이 실행되는 주소
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Authorization", "Content-Type")
             .allowCredentials(true);
