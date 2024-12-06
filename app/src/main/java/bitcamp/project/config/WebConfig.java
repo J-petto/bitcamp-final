1ef057b9df927fb6a8bd275af4f3c7518e1ca779
황민지 <hminji4565@gmail.com>
2024-12-06 18:49:52 +0900

    젠킨스 테스트를 위한 주석

--- app/src/main/java/bitcamp/project/config/WebConfig.java
+++ app/src/main/java/bitcamp/project/config/WebConfig.java
@@ -13,5 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Authorization", "Content-Type")
             .allowCredentials(true);
+        //test
     }
 }
